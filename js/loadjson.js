458180938ad07463546e1e3be7cab4ba0361a271
zhou haoxiang <835390899@qq.com>
2017-04-24 23:23:37 +0800

    index 2017年4月23日23:37:39

---
+++ js/loadjson.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+	$.ajax({
+		url:"JSON/zhxInfo.json",
+		type:"GET",
+		dataType:"json",
+		cache:false,
+		success:function(data){
+			var html='<ul>';
+			var json=eval(data);
+			$.each(json.studyRecord,function(index){
+				html+='<li>'+json.studyRecord[index].time+'<ul><li>'+json.studyRecord[index].info+'</li></ul></li>';
+			});
+			html+='<ul>';
+			$(".content").html(html);
+		}
+	});
+});
