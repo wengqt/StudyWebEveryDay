c48144a3ecb1879010673e0677e39abe2a476294
zhou haoxiang <835390899@qq.com>
2017-04-27 23:07:58 +0800

    2017年4月27日23:07:52

--- js/loadjson.js
+++ js/loadjson.js
@@ -1,17 +1,15 @@
-$(document).ready(function () {
-	$.ajax({
-		url:"JSON/zhxInfo.json",
-		type:"GET",
-		dataType:"json",
-		cache:false,
-		success:function(data){
-			var html='<ul>';
-			var json=eval(data);
-			$.each(json.studyRecord,function(index){
-				html+='<li>'+json.studyRecord[index].time+'<ul><li>'+json.studyRecord[index].info+'</li></ul></li>';
-			});
-			html+='<ul>';
-			$(".content").html(html);
-		}
-	});
+$.ajax({
+	url:"JSON/zhxInfo.json",
+	type:"GET",
+	dataType:"json",
+	cache:false,
+	success:function(data){
+		var html='<ul>';
+		var json=eval(data);
+		$.each(json.studyRecord,function(index){
+			html+='<li>'+json.studyRecord[index].time+'<ul><li>'+json.studyRecord[index].info+'</li></ul></li>';
+		});
+		html+='<ul>';
+		$(".content").html(html);
+	}
 });
--- js/loadjsonwithoutjq.js
+++ js/loadjsonwithoutjq.js
@@ -1,27 +1,28 @@
-// (function loadjson() {
-// 	var xmlhttp;
-// 	if (window.XMLHttpRequest) {
-// 		xmlhttp=new XMLHttpRequest();
-// 	}else{
-// 		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP")
-// 	}
-// 	xmlhttp.open('GET','JSON/zhxInfo.json',true);
-// 	xmlhttp.send();
-// 	xmlhttp.onreadystatechange=function(){
-// 		if (xmlhttp.readyState==4&&xmlhttp.status==200) {
-// 			var data=eval('('+xmlhttp.responseText+')');
-// 			var insertText='<ul>';
-// 			for(var i in data.studyRecord){
-//
-// 				insertText +='<li>'+data.studyRecord[i].time+'<ul><li>'+data.studyRecord[i].info+'</li></ul></li>';
-// 			}
-// 			insertText +='</ul>';
-// 			document.getElementById("content").innerHTML=insertText;
-//
-//
-// 		}
-//
-//
-//
-// 	}
-// })();
+(function loadjson() {
+	var xmlhttp;
+	if (window.XMLHttpRequest) {
+		xmlhttp=new XMLHttpRequest();
+	}else{
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP")
+	}
+	xmlhttp.open('GET','JSON/zhxInfo.json',true);
+	xmlhttp.send();
+	xmlhttp.onreadystatechange=function(){
+		if (xmlhttp.readyState==4&&xmlhttp.status==200) {
+			var data=eval('('+xmlhttp.responseText+')');
+			var insertText='<ul>';
+			for(var i in data.studyRecord){
+				
+				insertText +='<li>'+data.studyRecord[i].time+'<ul><li>'+data.studyRecord[i].info+'</li></ul></li>';
+			}
+			insertText +='</ul>';
+			document.getElementById("content").innerHTML=insertText;
+
+			
+		}
+
+
+
+	}
+})();
+
