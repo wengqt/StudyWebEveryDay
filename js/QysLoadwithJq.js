e513a019fcb181177609b2ea821f7f151e02aded
quyans@sina.com <quyans@sina.com>
2017-04-28 23:47:15 +0800

    曲延松：2017-4-28更新  写完了ajax但并不能实现

---
+++ js/QysLoadwithJq.js
@@ -0,0 +1,35 @@
+/**
+ * Created by 延松松松松 on 2017/4/28.
+ */
+window.onload =$.ajax({
+    url:"JSON/QYSinfo.json",
+    type:"GET",
+    dataType:"json",
+    success:function (data) {
+        for(var i=1;i<data.Study.length;i++){
+            var divTI = document.createElement("div");
+            divTI.className="task_item";
+            var divTN = document.createElement("div");
+            divTN.className = "task_name";
+            divTN.innerHTML= data.Study[i].time+data.Study[i].task_name;
+
+            var  divTA = document.createElement("div");
+            divTA.className = "task_according";
+            divTA.innerHTML=Childdata.Study[i].task_according;
+
+            divTI.appendChild(divTN);
+            divTI.appendChild(divTA);
+            document.getElementById("schedule").appendChild(divTI);
+
+
+
+
+
+
+
+        }
+
+    }
+
+
+});
